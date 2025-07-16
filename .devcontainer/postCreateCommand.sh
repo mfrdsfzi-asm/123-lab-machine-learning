234542ca6d6ff0b257848a213b1903f3e5d18f93
Firdaus Fauzi <firdaus.mdfauzi@bertelsmann.de>
2025-07-16 00:09:50 +0000

    initial commit

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pip install --upgrade pip
+pip3 install poetry ipykernel ipywidgets python-dotenv jupyter uv
+
+sudo apt-get update && sudo apt-get install ffmpeg libsm6 libxext6  -y
+
+
+
